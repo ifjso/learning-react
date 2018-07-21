f1d9f9e64a740319416a96be49c3bf670ce1bfb3
JesusOh <ojsboy@hotmail.com>
2018-07-21 19:35:48 +0900

    added config files

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "off",
+            "unix"
+        ],
+        "quotes": [
+            "off",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "off",
+            { "allow": ["warn", "error"] }
+        ],
+        "no-constant-condition": [
+            "error",
+            { "checkLoops": false }
+        ]
+    }
+};
