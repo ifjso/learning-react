75dec373605c90a88b9b1308896a463a0318302e
JesusOh <ojsboy@hotmail.com>
2018-08-12 17:46:00 +0900

    Add color organizer redux config files

---
+++ es6/chapter08/color-organizer-redux/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "dist", "assets"),
+        filename: "bundle.js",
+        sourceMapFilename: "bundle.map",
+    },
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_moudles)/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["env", "react", "stage-2"]
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize: true
+    }
+};
