a002490cb72c0b914082e29ff07a4e44c1cf2684
JesusOh <ojsboy@hotmail.com>
2018-08-15 12:32:25 +0900

    Remove semicolon

--- es6/chapter08/color-organizer-redux/webpack.config.js
+++ es6/chapter08/color-organizer-redux/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
     mode: "production",
@@ -24,4 +24,4 @@ module.exports = {
     optimization: {
         minimize: true
     }
-};
+}
