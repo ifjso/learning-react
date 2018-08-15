262536768f8e4aef5d3ab394d6c79222cf858e53
JesusOh <ojsboy@hotmail.com>
2018-08-15 20:06:15 +0900

    Completes React redux color organizer

---
+++ es6/chapter09/color-organizer/src/components/ui/ColorList.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import Color from './Color'
+import '../../../stylesheets/ColorList.scss'
+
+const ColorList = ({colors = [], onRemove = f => f, onRate = f => f}) =>
+    <div className="color-list">
+        {(colors.length === 0) ?
+            <p>No colors listed. (Add a color)</p> :
+            colors.map(color =>
+                <Color key={color.id} {...color}
+                    onRemove={() => onRemove(color.id)}
+                    onRate={rating => onRate(color.id, rating)} />
+            )
+        }
+    </div>
+
+Color.propTypes = {
+    colors: PropTypes.array,
+    onRate: PropTypes.func,
+    onRemove: PropTypes.func
+}
+
+export default ColorList
