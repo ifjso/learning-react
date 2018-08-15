8c0e234855a373eafbdf6c92a27b8a1bc418a244
JesusOh <ojsboy@hotmail.com>
2018-08-15 12:38:28 +0900

    Change and Apply 'SwitchCase' rule

--- es6/chapter08/color-organizer-redux/src/store/reducers.js
+++ es6/chapter08/color-organizer-redux/src/store/reducers.js
@@ -2,47 +2,47 @@ import C from '../constants'
 
 export const color = (state = {}, action) => {
     switch (action.type) {
-    case C.ADD_COLOR:
-        return {
-            id: action.id,
-            title: action.title,
-            color: action.color,
-            timestamp: action.timestamp,
-            rating: 0
-        }
-    case C.RATE_COLOR:
-        return (state.id !== action.id) ?
-            state :
-            {
-                ...state,
-                rating: action.rating
+        case C.ADD_COLOR:
+            return {
+                id: action.id,
+                title: action.title,
+                color: action.color,
+                timestamp: action.timestamp,
+                rating: 0
             }
-    default:
-        return state
+        case C.RATE_COLOR:
+            return (state.id !== action.id) ?
+                state :
+                {
+                    ...state,
+                    rating: action.rating
+                }
+        default:
+            return state
     }
 }
 
 export const colors = (state = [], action) => {
     switch (action.type) {
-    case C.ADD_COLOR:
-        return [
-            ...state,
-            color({}, action)
-        ]
-    case C.REMOVE_COLOR:
-        return state.filter(c => c.id !== action.id)
-    case C.RATE_COLOR:
-        return state.map(c => color(c, action))
-    default:
-        return state
+        case C.ADD_COLOR:
+            return [
+                ...state,
+                color({}, action)
+            ]
+        case C.REMOVE_COLOR:
+            return state.filter(c => c.id !== action.id)
+        case C.RATE_COLOR:
+            return state.map(c => color(c, action))
+        default:
+            return state
     }
 }
 
 export const sort = (state = "SORTED_BY_DATE", action) => {
     switch (action.type) {
-    case C.SORT_COLORS:
-        return action.sortBy
-    default:
-        return state
+        case C.SORT_COLORS:
+            return action.sortBy
+        default:
+            return state
     }
 }
