95e782e39c6c4e5d7588bcc05c74131650d30e5c
JesusOh <ojsboy@hotmail.com>
2018-08-26 16:33:50 +0900

    Add test for Color ui component

--- es6/chapter10/color-organizer/__tests__/components/ui/Color.test.js
+++ es6/chapter10/color-organizer/__tests__/components/ui/Color.test.js
@@ -2,7 +2,7 @@ import { compose } from 'redux'
 import toJSON from 'enzyme-to-json'
 import Color from '../../../src/components/ui/Color'
 
-const { shallow } = global.Enzyme
+const { shallow, mount } = global.Enzyme
 
 describe("<Color /> UI Component", () => {
 
@@ -13,4 +13,17 @@ describe("<Color /> UI Component", () => {
             <Color title="Test Color" color="#F0F0F0" rating={3} timestamp="Sun Aug 19 2018 20:01:03 GMT+0900 (PDT)"/>
         ).toMatchSnapshot()
     )
+
+    it("Invokes onRemove property", () => {
+        mount(<Color title="Test Color" color="#F0F0F0" />)
+            .find('button')
+            .simulate('click')
+    })
+
+    it("Invokes onRate property", () => {
+        mount(<Color title="Test Color" color="#F0F0F0" />)
+            .find('div.star-rating')
+            .childAt(4)
+            .simulate('click')
+    })
 })
