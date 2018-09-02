ec43a76ed7ae4384f403ab8a0278862ef636c9cb
JesusOh <ojsboy@hotmail.com>
2018-09-02 20:25:32 +0900

    Initial commit for company-website

---
+++ es6/chapter11/company-website/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "browser": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 9,
+        "ecmaFeatures": {            
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": ["react"],
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "off",
+            "unix"
+        ],
+        "quotes": [
+            "off",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": [
+            "off",
+            { "allow": [ "warn", "error" ] }
+        ],
+        "no-constant-condition": [
+            "error", 
+            { "checkLoops": false }
+        ],
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error"
+    }
+};
