db21d7529e8b40200f37c92ddcd218d1c4391bf0
JesusOh <ojsboy@hotmail.com>
2018-09-08 13:01:59 +0900

    Add redirect

--- es6/chapter11/company-website/src/index.js
+++ es6/chapter11/company-website/src/index.js
@@ -4,7 +4,8 @@ import { render } from 'react-dom'
 import {
     HashRouter,
     Route,
-    Switch
+    Switch,
+    Redirect
 } from 'react-router-dom'
 
 import {
@@ -24,6 +25,9 @@ render(
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/about" component={About} />
+                <Redirect from="/history" to="/about/history" />
+                <Redirect from="/services" to="/about/services" />
+                <Redirect from="/location" to="/about/location" />
                 <Route path="/events" component={Events} />
                 <Route path="/products" component={Products} />
                 <Route path="/contact" component={Contact} />
