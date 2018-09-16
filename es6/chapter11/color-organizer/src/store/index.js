a02384a5788cd52fcf5a11ba6926462ff1df0abf
JesusOh <ojsboy@hotmail.com>
2018-09-16 21:01:55 +0900

    Initial commit

---
+++ es6/chapter11/color-organizer/src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { colors, sort } from './reducers'
+import stateData from '../../data/initialState'
+
+const logger = store => next => action => {
+    console.groupCollapsed('dispatching', action.type)
+    console.log('prev state', store.getState())
+    console.log('action', action)
+    const result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd()
+    
+    return result
+}
+
+const saver = store => next => action => {
+    const result = next(action)
+    localStorage['redux-store'] = JSON.stringify(store.getState())
+    return result
+}
+
+const storeFactory = (initialData = stateData) =>
+    applyMiddleware(logger, saver)(createStore)(
+        combineReducers({colors, sort}),
+        (localStorage['redux-store']) ?
+            JSON.parse(localStorage['redux-store']) :
+            initialData
+    )
+
+export default storeFactory
