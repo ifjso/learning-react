20032549d6a6f101095ec686c83a71042e379ab4
JesusOh <ojsboy@hotmail.com>
2018-09-29 16:23:30 +0900

    Add isomorphic

---
+++ es6/chapter12/isomorphic-universal-samples/browser-load-and-print.js
@@ -0,0 +1,12 @@
+(() => {
+    const printNames = response => {
+        const people = JSON.parse(response).results,
+              names = people.map(({name}) => `${name.last}, ${name.first}`)
+        console.log(names.join('\n'))
+    }
+
+    const request = new XMLHttpRequest()
+    request.open('GET', 'https://api.randomuser.me?nat=US&results=10')
+    request.onload = () => printNames(request.response)
+    request.send()
+})()
